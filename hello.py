7b37ae987709a9d0f54cfa0ca111eef720701bac
echoqshen <90880564+echoqshen@users.noreply.github.com>
2022-02-14 23:38:28 -0500

    added random number

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import random
+from random import randrange
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +10,12 @@ def hello_world():
 @app.route("/salvador")
 def salvador():
     return "Hello, Salvador"
+
+@app.route("/random")
+def random():
+    
+    ret_val = "your random number is: " + Str(randrange(100))
+    return ret_val
 
 if __name__ == '__main__':
     app.run()
